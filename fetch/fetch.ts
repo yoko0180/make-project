5d35845f61615df0688673eec4acde9cd9ad03ff
yoko0180 <yoko0180@gmail.com>
2023-06-28 14:38:09 +0900

    fetchOutSingleRootUrlBase

--- fetch/fetch.ts
+++ fetch/fetch.ts
@@ -4,6 +4,11 @@ export const BASE_URL = "https://github.com/yoko0180/make-project/raw/master/tem
 
 export type Context = Record<string, unknown>
 type FetchOutSingle = {
+  url: string
+  outfilename: string
+  context: Context
+}
+type FetchOutSingleRootUrlBase = {
   urlRoot: string
   outfilename: string
   context: Context
@@ -14,18 +19,22 @@ type FetchOut = {
   context: Context
 }
 
-async function fetchOutSingle({ urlRoot, outfilename, context }: FetchOutSingle) {
+async function fetchOutSingle({ url, outfilename, context }: FetchOutSingle) {
   const outfile = await Deno.open(outfilename, { create: true, write: true, truncate: true })
-  const url = urlRoot + outfilename
   const w = outfile.writable
   await fetchTransform(url, w, context)
   w.close()
 }
 
+async function fetchOutSingleRootUrlBase({ urlRoot, outfilename, context }: FetchOutSingleRootUrlBase) {
+  const url = urlRoot + outfilename
+  await fetchOutSingle({url, outfilename, context})
+}
+
 export async function fetchOut({ names, urlRoot, context }: FetchOut) {
   const p = []
   for (const outfilename of names) {
-    p.push(fetchOutSingle({ urlRoot, outfilename, context }))
+    p.push(fetchOutSingleRootUrlBase({ urlRoot, outfilename, context }))
   }
   return await Promise.all(p)
 }
