20cfcd5987a0cb6d8b69538d8323eb3ac03f15b0
yoko0180 <yoko0180@gmail.com>
2023-07-05 12:43:12 +0900

    bin name

--- fetch/deno-cli.ts
+++ fetch/deno-cli.ts
@@ -1,12 +1,20 @@
+import { Input } from "https://deno.land/x/cliffy@v0.25.7/prompt/input.ts";
 import { BASE_URL, FetchArg, fetchOut } from "./fetch.ts"
 
-export async function fetchDenoCli(fetchArg: FetchArg) {
+export async function fetchDenoCli(fetchArgOpt: FetchArg) {
+  const binName = await Input.prompt("bin name?")
+  const fetchArg = {
+    ...fetchArgOpt,
+    context: {
+      ...fetchArgOpt.context,
+      binName
+    }
+  }
   await fetchOut({
     urlRoot: BASE_URL + "deno-base/",
     // prettier-ignore
     names: [
       ".vscode/settings.json",
-      "deno.jsonc",
       "main_bench.ts",
       "main_test.ts",
     ],
@@ -17,6 +25,7 @@ export async function fetchDenoCli(fetchArg: FetchArg) {
     // prettier-ignore
     names: [
       "main.ts",
+      "deno.jsonc",
     ],
     fetchArg
   })
